a36e7d55c5a7824bfc570082b86411ade31abf7d
yankai <yankailim00@gmail.com>
2022-04-07 11:16:12 +0800

    Update testing.java

--- the_project/src/reservation/testing.java
+++ the_project/src/reservation/testing.java
@@ -1,12 +1,19 @@
 package reservation;
 import java.time.Duration;
 import java.time.LocalDateTime;
+import room.*;
+import list_methods.*;
 
 public class testing {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		DateTime.getLocalDateTime("YOOOOO");
+		Room room = new SingleRoom(1,1);
+		try {
+			CreateReservation.createReservation(room);
+		}
+		catch (ArrayException e) {;}
 	}
 
 }
