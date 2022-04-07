9dd5fa80ea5c1f1384f24457f12be4c11ad454c8
yankai <yankailim00@gmail.com>
2022-04-07 12:25:51 +0800

    yk

--- the_project/src/hotel_app/HotelMainApp.java
+++ the_project/src/hotel_app/HotelMainApp.java
@@ -1,7 +1,22 @@
 package hotel_app;
+import java.util.Scanner;
 
 public class HotelMainApp {//randomcomment
+	
+	public static void main (String[] args ) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Welcome to the Hotel App System!");
+		System.out.println("1: Make a reservation");
+		System.out.println("2: Check in");
+		System.out.println("3: Check out");
+		int choice;
+		do {
+			System.out.println("Enter your choice:");
+			choice = sc.nextInt();
 
+			//add stuff
+		} while (choice<=3);
+	}
 	//lol
 	//Hi Tomoki
 }
