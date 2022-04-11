39a141f6e8c49048cbad3412ddef4115699131bd
Tomoki Teng <103061517+TomokiTeng@users.noreply.github.com>
2022-04-11 23:23:43 +0800

    fixed and javadoc

--- the_project/src/guest/CreditCardInfo.java
+++ the_project/src/guest/CreditCardInfo.java
@@ -1,13 +1,36 @@
 package guest;
 
 public class CreditCardInfo {
+	/**
+	 * this is the guest's credit card number
+	 */
 	private String creditCardNum;
+	/**
+	 * this is the guest's credit card bank
+	 */
 	private String creditCardBank;
+	/**
+	 * this is the guest's credit card expiry
+	 */
 	private String creditCardExpiry;
+	/**
+	 * this is the guest's billing address
+	 */
 	private String billingAddress;
+	/**
+	 * this is the guest's billing name
+	 */
 	private String billingName;
 	
 	//Constructor
+	/**
+	 * Instantiation of credit card information using the credit card number, bank, card expiry, billing address and billing name
+	 * @param CCN this is the guest's credit card number
+	 * @param CCB this is the guest's credit card bank
+	 * @param CCE this is the guest's credit card expiry
+	 * @param BA this is the guest's billing address
+	 * @param BN this is the guest's billing name
+	 */
 	public CreditCardInfo(String CCN, String CCB, String CCE, String BA, String BN) {
 		this.creditCardNum = CCN;
 		this.creditCardBank = CCB;
@@ -17,16 +40,56 @@ public class CreditCardInfo {
 	}
 	
 	//get methods
+	/**
+	 * gets the guest's credit card number
+	 * @return this is the guest's credit card number
+	 */
 	protected String getCreditCardNum(){return this.creditCardNum;}
+	/**
+	 * gets the guest's credit card bank
+	 * @return this is the guest's credit card bank
+	 */
 	protected String getCreditCardBank(){return this.creditCardBank;}
+	/**
+	 * gets the guest's credit card expiry
+	 * @return this is the guest's credit card expiry
+	 */
 	protected String creditCardExpiry(){return this.creditCardExpiry;}
+	/**
+	 * gets the guest's credit card billing address
+	 * @return this is the guest's credit card billing address
+	 */
 	protected String billingAddress(){return this.billingAddress;}
+	/**
+	 * gets the guest's credit card billing name
+	 * @return this is the guest's credit card billing name
+	 */
 	protected String billingName(){return this.billingName;}
 	
 	//set methods
+	/**
+	 * sets the credit card number of the guest
+	 * @param CCN this is the guest credit card number
+	 */
 	protected void setCreditCardNum(String CCN) {this.creditCardNum = CCN;}
+	/**
+	 * sets the credit card bank of the guest
+	 * @param CCN this is the guest credit card bank
+	 */
 	protected void setCreditCardBank(String CCB) {this.creditCardBank = CCB;}
+	/**
+	 * sets the credit card expiry of the guest
+	 * @param CCN this is the guest credit card expiry
+	 */
 	protected void setCreditCardExpiry(String CCE) {this.creditCardExpiry = CCE;}
+	/**
+	 * sets the credit card billing address of the guest
+	 * @param CCN this is the guest credit card billing address
+	 */
 	protected void setBillingAddress(String BA) {this.billingAddress = BA;}
-	protected void setBillingName(String BN) {this.billingAddress = BN;}
+	/**
+	 * sets the credit card billing name of the guest
+	 * @param CCN this is the guest credit card billing name
+	 */
+	protected void setBillingName(String BN) {this.billingName = BN;}
 }
