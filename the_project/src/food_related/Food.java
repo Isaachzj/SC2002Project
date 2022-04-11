94b2a751918457219c3eaf3b88856161e9df8395
Tomoki Teng <103061517+TomokiTeng@users.noreply.github.com>
2022-04-11 14:26:39 +0800

    Include Javadocs code

--- the_project/src/food_related/Food.java
+++ the_project/src/food_related/Food.java
@@ -1,11 +1,29 @@
 package food_related;
 
 public class Food {
+	
+	/**
+	 * name of food item
+	 */ 
 	private String foodName;
+	
+	/**
+	 * description of food item
+	 */ 
 	private String description;	
+	
+	/**
+	 * price of food item
+	 */ 
 	private double price;
 
 	//Constructor
+	/**
+	 * creates a Food object with input name, price and description
+	 * @param name name of this food item
+	 * @param price price of this food item
+	 * @param description description of this food item
+	 */ 
 	public Food(String name, double price, String description) {
 		this.foodName = name;
 		this.price = price;
@@ -13,11 +31,28 @@ public class Food {
 	}
 	
 	//get methods
+	/**
+	 * @return name of this food item
+	 */ 
 	protected String getFoodName() {return this.foodName;}
-	protected String getDescription() {return this.description;}	
+	/**
+	 * @return description of this food item
+	 */ 
+	protected String getDescription() {return this.description;}
+	/**
+	 * @return price of this food item
+	 */ 
 	protected double getPrice() {return this.price;}
 	
 	//set method
+	/**
+	 * sets price of this food item
+	 * @param price price of this food item
+	 */ 
 	protected void setPrice(double price) {this.price = price;}
+	/**
+	 * sets description of this food item
+	 * @param description description of this food item
+	 */ 
 	protected void setDescription(String description) {this.description = description;}
 }
