cb3d1e085d5d4b81cc62cd1d6e951c219338f50e
Tomoki Teng <103061517+TomokiTeng@users.noreply.github.com>
2022-04-11 17:56:27 +0800

    Design principles added

--- the_project/src/food_related/DisplayMenu.java
+++ the_project/src/food_related/DisplayMenu.java
@@ -4,8 +4,10 @@ import list_methods.*;
 public class DisplayMenu implements Print {
 	private Menu menu;
 	/**
-	 * creates a DisplayMenu object and
-	 * adds this menu into this DisplayMenu 
+	 * creates a DisplayMenu object and adds this menu into this DisplayMenu
+	 * The DisplayMenu class follows the Single Responsibility principle, as has a sole responsibility of displaying the Menu 
+	 * Additionally; it follows the Open-Closed principle as changes in the Display does not require a change in source code of modules
+	 * (i.e. open for extensions but not modifications)
 	 * @param menuList Menu object
 	 */ 
 	public DisplayMenu(Menu menuList) {
@@ -14,7 +16,8 @@ public class DisplayMenu implements Print {
 	
 	//Overridding printArray() method in Print
 	/**
-	 * prints food items, index of food items, price of food items and description of food items in this menu
+	 * Overrides the printArray() method in Print
+	 * this prints food items, index of food items, price of food items and description of food items in this menu
 	 */ 
 	public void printArray() {
 		
