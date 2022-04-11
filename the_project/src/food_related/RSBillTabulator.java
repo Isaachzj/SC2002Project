7a70971e0679b89b46a8364b44250b7185e5c64d
Tomoki Teng <103061517+TomokiTeng@users.noreply.github.com>
2022-04-11 15:39:27 +0800

    Include javadocs

--- the_project/src/food_related/RSBillTabulator.java
+++ the_project/src/food_related/RSBillTabulator.java
@@ -3,10 +3,19 @@ package food_related;
 public class RSBillTabulator {
 	private RoomService rs;
 	
+	/**
+	 * creates a RSBillTabulator object and
+	 * adds this room service order into this RSBillTabulator 
+	 * @param rs RSBillTabulator object
+	 */
 	public RSBillTabulator(RoomService rs) {
 		this.rs = rs;
 	}
 	
+	/**
+	 * sums up all food item prices into totalBill 
+	 * and sets totalBill as RoomServiceBill in this room service order
+	 */
 	public void calculateRoomServiceBill() {
 		double totalBill=0.0;
 		
