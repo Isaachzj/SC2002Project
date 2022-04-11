a9167bbe47f9812cc9a5f0582bc006602dfac996
Tomoki Teng <103061517+TomokiTeng@users.noreply.github.com>
2022-04-11 18:00:47 +0800

    Principles updated

--- the_project/src/food_related/BillTabulator.java
+++ the_project/src/food_related/BillTabulator.java
@@ -4,13 +4,17 @@ public class BillTabulator {
 	private Order order;
 	/**
 	 * Returns the Guest's order bill
-	 * @return this is the order bill of the Guest
+	 * Follows Single Responsibility principle as it has the single responsibility of returning Guest's order bill
+	 * Also follows the Open-Close principle since software entity in this case open for extension but closed for modifications
+	 * @param this is the order bill of the Guest
 	*/
 	public BillTabulator(Order order) {
 		this.order = order;
 	}
 	/**
 	 * Calculates the total bill of guest by tabulating the prices of food ordered by the respective guest
+	 * Similarly, also follows the Single Responsibility principle with sole responsibility of calculating the Bill
+	 * Also follows the Open-Close principle since software entity in this case open for extension but closed for modifications
 	*/
 	public void calculateBill() {
 		for (int i=0; i<order.getArray().size(); i++) {
