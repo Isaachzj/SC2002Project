3da98b36a14d23cb15d74ffb26e8278add081bf2
Tomoki Teng <103061517+TomokiTeng@users.noreply.github.com>
2022-04-11 14:20:14 +0800

    Include Javadocs code

--- the_project/src/food_related/BillTabulator.java
+++ the_project/src/food_related/BillTabulator.java
@@ -2,11 +2,16 @@ package food_related;
 
 public class BillTabulator {
 	private Order order;
-	
+	/**
+	 * Returns the Guest's order bill
+	 * @return this is the order bill of the Guest
+	*/
 	public BillTabulator(Order order) {
 		this.order = order;
 	}
-	
+	/**
+	 * Calculates the total bill of guest by tabulating the prices of food ordered by the respective guest
+	*/
 	public void calculateBill() {
 		for (int i=0; i<order.getArray().size(); i++) {
 			
