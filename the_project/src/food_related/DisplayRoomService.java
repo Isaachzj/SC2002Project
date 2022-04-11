abe1c1a1a085aecc9b6ef02062ccfbb1d015e3f7
Tomoki Teng <103061517+TomokiTeng@users.noreply.github.com>
2022-04-11 14:24:12 +0800

    Include Javadocs code

--- the_project/src/food_related/DisplayRoomService.java
+++ the_project/src/food_related/DisplayRoomService.java
@@ -3,11 +3,18 @@ import list_methods.Print;
 
 public class DisplayRoomService implements Print {
 	private RoomService rs;
-	
+	/**
+	 * Displays the Room service of the guests
+	 * @param    rs this is the Room service of the guests
+	*/
 	public DisplayRoomService(RoomService rs) {
 		this.rs = rs;
 	}
-	
+	/**
+	 * Prints out the details of orders and the total bill for room service
+	 * The number of orders taken would be printed out if the order size if non-zero. Else, it would indicate that no orders were taken.
+	 * Will also display the total bill for the room service
+	*/
 	public void printArray() {
 		//System.out.println("=============== Order ===============");
 		if (rs.getArray().size()==0) {
