b270dbf22d9209fb30fd449ae7418633b78b2b24
Tomoki Teng <103061517+TomokiTeng@users.noreply.github.com>
2022-04-13 02:44:18 +0800

    (explanation)

--- the_project/src/reservation/GrandTotalTabulator.java
+++ the_project/src/reservation/GrandTotalTabulator.java
@@ -4,15 +4,27 @@ import room.*;
 import room_rate.*;
 
 public class GrandTotalTabulator {
+	/**
+	 * grandTotal method calculates the total bill incurred under the Reservation object.
+	 * The total bill incurred includes the roomBill and roomServiceBill
+	 * roomBill consist of the bills incurred during the guest's stay; such as Food items ordered from the Menu
+	 * roomServiceBill consist of the bill incurred given the room booked and number of weekdays and weekends stayed
+	 * @param reservation this is the Reservation object for the tabulation of the total bill incurred
+	 * @return this is the total bill incurred through the stay in the Hotel
+	 */
 	public static double grandTotal(Reservation reservation) {
 		Room room = reservation.getRoom();
 		
-		// Getting Room Bill based on length of stay
+		/**
+		 * # Getting Room Bill based on length of stay
+		 */
 		long numOfWeekdays = reservation.getNumOfWeekday();
 		long numOfWeekends = reservation.getNumOfWeekend();
 		double roomBill = RoomBillTabulator.calculateRoomBill(room, numOfWeekdays, numOfWeekends);
 		
-		//Getting total bill for orders
+		/**
+		 * # Getting total bill for orders
+		 */
 		double roomServiceBill = room.getRoomService().getRoomServiceBill();
 		
 		return roomBill + roomServiceBill;
