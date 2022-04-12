65db64450833ed5965040c37ecca68dcae0f0a10
Tomoki Teng <103061517+TomokiTeng@users.noreply.github.com>
2022-04-12 14:43:10 +0800

    grammar

--- the_project/src/list_methods/Add.java
+++ the_project/src/list_methods/Add.java
@@ -2,7 +2,7 @@ package list_methods;
 /**
  * Follows the Interface Segregation Principle
  * Many smaller interface; inwhich this case is an extension of AddStandard and AddGivenObject class
- * Allows an object to be added as an entry
+ * Allows an object to be added to an entry
  * AddStandard throws in an Exception when duplicate entry is spotted upon adding of new entry
  * AddGivenObject calls addEntry method which allows object to be added given no duplicate entry found
  * 
