5a00918c45664f9a306ab9c685e94220ca57a67e
Tomoki Teng <103061517+TomokiTeng@users.noreply.github.com>
2022-04-12 14:50:05 +0800

    minor change

--- the_project/src/list_methods/Print.java
+++ the_project/src/list_methods/Print.java
@@ -1,5 +1,9 @@
 package list_methods;
 
 public interface Print {
+	/**
+	 * calls printArray() method which prints the objects in the list (with exception would be thrown for any error message encountered)
+	 * @throws ArrayException occurs when there are any errors upon the printing of objects in an array
+	 */
 	public void printArray() throws ArrayException;
 }
