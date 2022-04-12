f0a16bd71b99914013f391b5996aa2987929b133
Tomoki Teng <103061517+TomokiTeng@users.noreply.github.com>
2022-04-12 12:35:01 +0800

    Exception Handling

--- the_project/src/list_methods/Get.java
+++ the_project/src/list_methods/Get.java
@@ -2,5 +2,11 @@ package list_methods;
 
 public interface Get {
 	//get method
+	/**
+	 * get method
+	 * to get an object from entry
+	 * @return this is the Exception handling message
+	 * @throws ArrayException occurs upon an error upon get method from entry
+	 */
 	public Object getEntry() throws ArrayException;
 }
