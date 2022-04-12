5eae51f4926ab7646b840bec13edac1f861a2756
Tomoki Teng <103061517+TomokiTeng@users.noreply.github.com>
2022-04-12 15:24:54 +0800

    Explanation of enum parts + minor grammar chaneg

--- the_project/src/enumeration/AvailStatus.java
+++ the_project/src/enumeration/AvailStatus.java
@@ -1,5 +1,17 @@
 package enumeration;
-
+/**
+ * For the enum of AvailStatus; there are 4 types available (VACANT, OCCUPIED, RESERVED, UNDER_MAINTENANCE)
+ * VACANT is used to indicate where the room is open for reservation; where room is unoccupied and not reserved
+ * OCCUPIED is used to indicate where guest has already checked into the room; where room is occupied
+ * RESERVED is used to indicate where the room has been reserved for check in at the given time
+ * if however, guest do not check in by the stipulated time, AvailStatus of room will be changed to VACANT to allow for other reservations
+ * UNDER_MAINTENANCE is used to indicate when a room is held either for renovation or room cleaning and hence would not be open for reservation
+ * however, once the maintenance for the room is completed, room status can be changed back to VACANT
+ * 
+ * @author Isaac, Yan kai, Davis, Wenlu, Tomoki
+ * @version 7.0
+ * @since 2022-03-15
+ */
 public enum AvailStatus {
 	VACANT, OCCUPIED, RESERVED, 
 	UNDER_MAINTENANCE
--- the_project/src/enumeration/OrderStatus.java
+++ the_project/src/enumeration/OrderStatus.java
@@ -1,5 +1,12 @@
 package enumeration;
 
 public enum OrderStatus {
+	/**
+	 * For the enumeration of the OrderStatus, there are 3 types (CONFIRMED, PREPARING, DELIVERED)
+	 * 
+	 * CONFIRMED is used to indicate when the order for the Food object has been received
+	 * PREPARING is used to indicate when the order for the Food object is under preparation or admist delivery
+	 * DELIVERED is used to indicate when the food has been delivered to the guest
+	 */
 	CONFIRMED, PREPARING, DELIVERED
 }
