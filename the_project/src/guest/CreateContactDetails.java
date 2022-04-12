3dbbe5abd6c860088142eb46fab81cd3d7721735
Tomoki Teng <103061517+TomokiTeng@users.noreply.github.com>
2022-04-12 09:27:04 +0800

    minor inclusion

--- the_project/src/guest/CreateContactDetails.java
+++ the_project/src/guest/CreateContactDetails.java
@@ -5,7 +5,7 @@ public class CreateContactDetails {
 	/**
 	 * instantiation of guest contact details; using their user input mobile number, email address, home address and country of origin
 	 * once the contact details of guest has been instantiated, it would be returned
-	 * @return this is the contact details of the guest
+	 * @return this is the contact details of the guest instantiated
 	 */
 	public static ContactDetails createContactDetails() {
 		Scanner sc = new Scanner(System.in);
