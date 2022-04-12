009746ca4610b81547fc31b655e261640d2d7e6c
Tomoki Teng <103061517+TomokiTeng@users.noreply.github.com>
2022-04-12 14:44:35 +0800

    Exception handling

--- the_project/src/list_methods/Remove.java
+++ the_project/src/list_methods/Remove.java
@@ -1,5 +1,15 @@
 package list_methods;
-
+/**
+ * Follows the Interface Segregation Principle
+ * Many smaller interface; inwhich this case is an extension of RemoveStandard and RemoveGivenObject class
+ * Allows an object to be removed from the entry
+ * RemoveStandard throws in an Exception when no entry is found upon its request of entry removal
+ * RemoveGivenObject calls removeEntry method which allows object to be removed given entry found
+ * 
+ * @author Isaac, Yan kai, Davis, Wenlu, Tomoki
+ * @version 7.0
+ * @since 2022-03-15
+ */
 public interface Remove extends RemoveStandard, RemoveGivenObject{
 
 }
