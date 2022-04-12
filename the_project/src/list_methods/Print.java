d4bb3bab3ac3550214f452f2302c319aadf26907
Tomoki Teng <103061517+TomokiTeng@users.noreply.github.com>
2022-04-12 14:51:55 +0800

    explanation of interface

--- the_project/src/list_methods/Print.java
+++ the_project/src/list_methods/Print.java
@@ -2,7 +2,7 @@ package list_methods;
 
 public interface Print {
 	/**
-	 * calls printArray() method which prints the objects in the list (with exception would be thrown for any error message encountered)
+	 * calls printArray() method which prints the objects in the array (with exception would be thrown for any error message encountered)
 	 * @throws ArrayException occurs when there are any errors upon the printing of objects in an array
 	 */
 	public void printArray() throws ArrayException;
--- the_project/src/list_methods/Reset.java
+++ the_project/src/list_methods/Reset.java
@@ -1,5 +1,9 @@
 package list_methods;
 
 public interface Reset {
+	/**
+	 * calls resetArray() method which resets the array (with exception would be thrown for any error message encountered)
+	 * @throws ArrayException occurs when there are any errors upon the reset of the array
+	 */
 	public void resetArray() throws ArrayException;
 }	
