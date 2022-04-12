12d86797c8a8ec1f91dc135ae97fc08afa9d167f
Tomoki Teng <103061517+TomokiTeng@users.noreply.github.com>
2022-04-12 14:38:57 +0800

    Exception handling n similar to AddGivenObject

--- the_project/src/list_methods/RemoveGivenObject.java
+++ the_project/src/list_methods/RemoveGivenObject.java
@@ -1,5 +1,10 @@
 package list_methods;
 
 public interface RemoveGivenObject {
+	/**
+	 * calls removeEntry method which allows object to be added given no exception thrown (i.e. given no errors)
+	 * @param object this is the object to be removed from the Entry
+	 * @throws ArrayException occurs upon an error and error message would be printed accordingly
+	 */
 	public void removeEntry(Object object) throws ArrayException;
 }
