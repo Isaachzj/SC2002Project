5fa607091e43499c505dedfed01226e3430dc9d2
Tomoki Teng <103061517+TomokiTeng@users.noreply.github.com>
2022-04-13 03:13:45 +0800

    Instantiation and explanation

--- the_project/src/reservation/Reservation.java
+++ the_project/src/reservation/Reservation.java
@@ -10,12 +10,27 @@ public class Reservation {
 	Scanner sc = new Scanner(System.in);
 	protected Room room;
 	
-
-	protected LocalDateTime checkInDateTime; 
-	protected LocalDateTime checkOutDateTime; 
+	/**
+	 * this is the check-in date and time (protected attributes)
+	 */
+	protected LocalDateTime checkInDateTime;
+	/**
+	 * this is the check-out date and time (protected attributes)
+	 */
+	protected LocalDateTime checkOutDateTime;
+	/**
+	 * this is the number of weekday for the booking by the guest (protected attributes)
+	 */
 	protected long numOfWeekday;
+	/**
+	 * this is the number of weekend for the booking by the guest (protected attributes)
+	 */
 	protected long numOfWeekend;
+	/**
+	 * this is the number of guests (protected attributes)
+	 */
 	protected int numOfGuest;
+	
 	protected ArrayList<Guest> guestList = new ArrayList<Guest>();
 	
 	/*public Reservation(Room room) {  (CAN DELETE SINCE NOT USING)
@@ -25,7 +40,19 @@ public class Reservation {
 		this.numOfGuest = 0;
 		this.guestList = new ArrayList<Guest>();
 	}	*/
-	
+		
+		/**
+		 * Instantiation of Reservation object
+		 * this includes details such as the room, check-in date-time, check-out date-time, number of weekday and weekend of stay, number of guest
+		 * @param room room this is the Room object of the reservation
+		 * @param checkInDateTime this is the check in date of the reservation.
+		 * The check in date will always be before the check out date.
+		 * @param checkOutDateTime this is the check out date of the reservation.
+		 * The check out date will always be after the check in date.
+		 * @param numOfWeekday this is the number of weekdays for the stay
+		 * @param numOfWeekend this is the number of weekends for the stay
+		 * @param numOfGuest this is the number of guest that would be staying in the hotel for the reservation
+		 */
 		public Reservation(Room room, LocalDateTime checkInDateTime, LocalDateTime checkOutDateTime, long numOfWeekday, long numOfWeekend, int numOfGuest) {
 		this.room = room;
 		this.numOfWeekday = numOfWeekday;
@@ -35,21 +62,72 @@ public class Reservation {
 		this.checkOutDateTime = checkOutDateTime;
 	}
 	
-	
+	/**
+	 * method returning the Room object is public
+	 * @return this is the room with the details.
+	 * Details includes type of room, availability, feature, room number, occupancy and room rate
+	 */
 	public Room getRoom() {return this.room;}
+	/**
+	 * method returning the Check-in date and time is public.
+	 * @return this is the check in date.
+	 * The check in date will always be before the check out date.
+	 */
 	public LocalDateTime getCheckInDateTime() {return this.checkInDateTime;}
+	/**
+	 * method returning the Check-out date and time is public
+	 * @return this is the check out date
+	 * The check in date will always be before the check out date.
+	 */
 	public LocalDateTime getCheckOutDateTime() {return this.checkOutDateTime;}
+	/**
+	 * method returning the number of weekday is public
+	 * @return this is the number of weekday
+	 * weekdays are days falling between monday to friday
+	 */
 	public long getNumOfWeekday() {return this.numOfWeekday;}
+	/**
+	 * method returning the number of weekend is public
+	 * @return this is the number of weekend
+	 * weekdays are days falling on saturday or sunday
+	 */
 	public long getNumOfWeekend() {return this.numOfWeekend;}
+	/**
+	 * method returning the arraylist of guests is public
+	 * @return this is the arraylist of guests
+	 */
 	public ArrayList<Guest> getGuestList() {return this.guestList;}
+	/**
+	 * method returning the number of guests is public
+	 * @return this is the number of guests
+	 */
 	public int getNumOfGuest() {return this.numOfGuest;}
+	/**
+	 * method returning the first guest in the guestlist arraylist is public
+	 * @return this is the first guest in the guest arraylist
+	 */
 	public Guest getGuest() {return this.guestList.get(0);}	
-
+	
+	/**
+	 * setCheckInDateTime checks if the check in date and time is the same
+	 */
 	public void setCheckInDateTime(LocalDateTime checkInDateTime) {this.checkInDateTime = checkInDateTime;}
-	public void setCheckOutDateTime(LocalDateTime checkOutDateTime) {this.checkOutDateTime = checkOutDateTime;} 
+	/**
+	 * setCheckOutDateTime checks if the check out date and time is the same
+	 */
+	public void setCheckOutDateTime(LocalDateTime checkOutDateTime) {this.checkOutDateTime = checkOutDateTime;}
+	/**
+	 * setNumOfWeekday checks if the number of weekday is the same
+	 */
 	public void setNumOfWeekday(long numOfWeekday) {this.numOfWeekday = numOfWeekday;}
+	/**
+	 * setNumOfWeekday checks if the number of weekend is the same
+	 */
 	public void setNumOfWeekend(long numOfWeekend) {this.numOfWeekend = numOfWeekend;}
 	
+	/**
+	 * addGuest() method allows the Guest object to be added to the entry
+	 */
 	public void addGuest() {
 		ReservationGuestListManipulator rglm = new ReservationGuestListManipulator(this);
 		rglm.addEntry();
--- the_project/src/reservation/ReservationGuestListManipulator.java
+++ the_project/src/reservation/ReservationGuestListManipulator.java
@@ -7,13 +7,22 @@ import java.util.Scanner;
 
 public class ReservationGuestListManipulator implements AddStandard{
 	private Reservation reservation;
-	
+	/**
+	 * gets the Reservation object
+	 * @param reservation this is the Reservation object
+	 */
 	public ReservationGuestListManipulator(Reservation reservation) {
 		this.reservation = reservation;
 	}
 	
+	/**
+	 * Instantiation of reserver's details and the guest's details
+	 * addEntry() method is also public
+	 */
 	public void addEntry() {
-		//Create reserver
+		/**
+		 * # Create reserver
+		 */
 		System.out.println("\n------------ Reserver's details ------------");
 		Guest reserver = CreateGuest.createReserver(reservation);
 		reservation.getGuestList().add(reserver);
