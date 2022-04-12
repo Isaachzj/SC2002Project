1fc9fffc2b6f5ad6d869bf2ab9715d863a3cb066
Tomoki Teng <103061517+TomokiTeng@users.noreply.github.com>
2022-04-12 11:58:14 +0800

    method uses explained + param purpose

--- the_project/src/food_related/RSBillTabulator.java
+++ the_project/src/food_related/RSBillTabulator.java
@@ -4,9 +4,11 @@ public class RSBillTabulator {
 	private RoomService rs;
 	
 	/**
+	 * Adds RoomService bills to calculate the total bill tabulated
 	 * Follows the Single Responsibility principle due to sole responsibility
 	 * It also follows the Open-Closed principle due to its software entities open for extension but closed for modifications
-	 * @param rs RSBillTabulator object
+	 * @param rs RSBillTabulator object;
+	 * the RoomService object includes the list of food items ordered and its price, which is required for this method to calculate the total bill of the RoomService
 	 */
 	public RSBillTabulator(RoomService rs) {
 		this.rs = rs;
@@ -23,7 +25,9 @@ public class RSBillTabulator {
 			totalBill += curOrder.getBill();
 		}
 		
-		//set the total bill of room service
+		/**
+		 * set the total bill of room service
+		 */
 		rs.setRoomServiceBill(totalBill);
 	}
 }
