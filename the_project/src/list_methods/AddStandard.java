096609b95f680f1f73055ff70277afb59aee4638
Tomoki Teng <103061517+TomokiTeng@users.noreply.github.com>
2022-04-12 12:25:10 +0800

    Exception handling

--- the_project/src/list_methods/AddStandard.java
+++ the_project/src/list_methods/AddStandard.java
@@ -1,5 +1,10 @@
 package list_methods;
 
 public interface AddStandard {
+	/**
+	 * Exception handling for duplicate entry
+	 * i.e. when a duplicate entry is found upon adding of entry; exception (ArrayException) would be thrown
+	 * @throws ArrayException
+	 */
 	public void addEntry() throws ArrayException;	//exception for duplicate entry
 }
