f051426330ec45417a436b967633858fac4dc5bd
Tomoki Teng <103061517+TomokiTeng@users.noreply.github.com>
2022-04-12 12:22:24 +0800

    Author + ISP and explanation of extended classes

--- the_project/src/list_methods/Add.java
+++ the_project/src/list_methods/Add.java
@@ -1,5 +1,15 @@
 package list_methods;
-
+/**
+ * Follows the Interface Segregation Principle
+ * Many smaller interface; inwhich this case is an extension of AddStandard and AddGivenObject class
+ * AddStandard throws in an Exception when duplicate entry is spotted upon adding of new entry
+ * AddGivenObject calls addEntry method which allows object to be added given no duplicate entry found
+ * 
+ * @author Isaac, Yan kai, Davis, Wenlu, Tomoki
+ * @version 7.0
+ * @since 2022-03-15
+ *
+ */
 public interface Add extends AddStandard, AddGivenObject{
 
 }
