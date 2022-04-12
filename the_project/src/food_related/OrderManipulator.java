e1ba2c1ffb1b7cb6b8ff7a6440dd7244bee5b125
Tomoki Teng <103061517+TomokiTeng@users.noreply.github.com>
2022-04-12 11:40:12 +0800

    Instantiation and extra explanation

--- the_project/src/food_related/OrderManipulator.java
+++ the_project/src/food_related/OrderManipulator.java
@@ -4,7 +4,10 @@ import list_methods.*;
 
 public class OrderManipulator implements AddGivenObject{
 	private Order order;
-
+	
+	/**
+	 * # Constructor
+	 */
 	/**
      * This Constructor follows the Single Responsibility principle
 	 * Additionally, it follows the Open-Closed principle as its software entities is open for extension but closed for modifications
@@ -15,19 +18,20 @@ public class OrderManipulator implements AddGivenObject{
 	}
 	
 	/**
-	 * Includes the ordered food into the order food list
-	 * If ordered food is either null or not an instance of Food class, then it will throw in an exception with an Error displayed to inform user that the object passed to this function is not a food object
-	 * Once it is confirmed that object passed to function is a food object, quantity of food to be included. System also ensures that quantity of food entered is a positive number
+	 * Adds the ordered food into the order food list
+	 * Exception would be thrown if ordered food is either null or not an instance of Food class, with an Error displayed to inform user that the object passed to this function is not a food object
+	 * Else, object passed to function is a food object, quantity of food to be included. System also ensures that quantity of food entered is a positive number
 	 * 
 	 * @param   daFood this is the food ordered by the guest
 	*/
-	// Adds the ordered food into the order food list
 	public void addEntry(Object daFood) throws ArrayException{
 		boolean found = false;
 		int index=0;
 		Food food;
 		
-		//For this case, we will actually ensure that only a food object is passed to this function so this is actually not needed but we kiasu :)
+		/**
+		 * For this case, we will actually ensure that only a food object is passed to this function so this is actually not needed but we kiasu :)
+		 */
 		if (daFood==null) {throw new ArrayException("Error!!");}
 		else if (!(daFood instanceof Food)) { throw new ArrayException("Error!!");}
 		else {food = (Food) daFood;} 
@@ -38,13 +42,17 @@ public class OrderManipulator implements AddGivenObject{
 		int quantity = sc.nextInt();
 		sc.nextLine();
 		
-		//Ensure quantity is greater than 0
+		/**
+		 * Ensure quantity is greater than 0
+		 */
 		while (quantity<=0) {
 			System.out.println("Enter valid quantity");
 			quantity = sc.nextInt(); sc.nextLine();
 		}
 		
-		//Check if such an entry exists!
+		/**
+		 * Check if such an entry exists!
+		 */
 		for (int i=0; i<order.getArray().size(); i++) {
 			Food disfood = order.getArray().get(i);
 			if (disfood.getFoodName().compareTo(food.getFoodName()) == 0) {
@@ -53,12 +61,16 @@ public class OrderManipulator implements AddGivenObject{
 			}//end if
 		}//end for
 		
-		//Update quantity if food added before
+		/**
+		 * Update quantity if food added before
+		 */
 		if (found) {
 			order.getQuantity().set(index, order.getQuantity().get(index) + quantity);
 			System.out.println("Order Added successfully!");
 		}
-		//Add new food entry and quantity if not ordered before
+		/**
+		 * Add new food entry and quantity if not ordered before
+		 */
 		else {
 			order.getArray().add(food);
 			order.getQuantity().add(quantity);
