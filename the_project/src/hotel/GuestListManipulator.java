d89bfdcb22d77429234ba4bfb6cc668c9721a153
Tomoki Teng <103061517+TomokiTeng@users.noreply.github.com>
2022-04-12 16:42:14 +0800

    to be continued! i go take a break first lol

--- the_project/src/hotel/GuestListManipulator.java
+++ the_project/src/hotel/GuestListManipulator.java
@@ -6,12 +6,17 @@ import list_methods.*;
 import java.util.*;
 
 public class GuestListManipulator implements AddGivenObject, RemoveGivenObject {
+	/**
+	 * this is the Hotel object (private attribute)
+	 */
 	private Hotel hotel;
 	
 	public GuestListManipulator(Hotel hotel) {
 		this.hotel = hotel;
 	}
-	
+	/**
+	 * Adds the specified entry from the parameter into the Reservation
+	 */
 	public void addEntry(Object daReservation) throws ArrayException{
 		Reservation reservation;
 		
@@ -19,14 +24,18 @@ public class GuestListManipulator implements AddGivenObject, RemoveGivenObject {
 		else if (!(daReservation instanceof Reservation)) { throw new ArrayException("Error!!");}
 		else {reservation = (Reservation) daReservation;} 
 		
-		//Traversing array of guest in reservation and adding them to Hotel Guest List
+		/**
+		 * Traversing array of guest in reservation and adding them to Hotel Guest List
+		 */
 		for (int i=0; i<reservation.getGuestList().size(); i++) {
 			Guest guest = reservation.getGuestList().get(i);
 			hotel.getGuestList().add(guest);	
 		}
 	}
 	
-	//Checkout function will pass all guest object stored in reservation to this to remove
+	/**
+	 * Checkout function will pass all guest object stored in reservation to this to remove
+	 */
 	public void removeEntry(Object daReservation) throws ArrayException{
 		Reservation reservation;
 		
@@ -34,7 +43,9 @@ public class GuestListManipulator implements AddGivenObject, RemoveGivenObject {
 		else if (!(daReservation instanceof Reservation)) { throw new ArrayException("Error!!");}
 		else {reservation = (Reservation) daReservation;} 
 		
-		//Traversing array of guest in reservation and adding them to Hotel Guest List
+		/**
+		 * Traversing array of guest in reservation and adding them to Hotel Guest List
+		 */
 		for (int i=0; i<reservation.getGuestList().size(); i++) {
 			Guest guest = reservation.getGuestList().get(i);
 			hotel.getGuestList().remove(guest);	
@@ -42,7 +53,12 @@ public class GuestListManipulator implements AddGivenObject, RemoveGivenObject {
 	}
 
 
-	//Asks for guest name and mobile number and retrieves the correct Guest object
+	/**
+	 * Retrieves Guest object using mobile number and name
+	 * Asks for guest name and mobile number and retrieves Guest object accordingly
+	 * @return the Guest object would be returned using the getEntry() method - if it is found in the guest list;
+	 * else, null would be returned if the Guest object is not found in the guest list
+	 */
 	public Guest getEntry() {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the Guest name:");
@@ -50,7 +66,9 @@ public class GuestListManipulator implements AddGivenObject, RemoveGivenObject {
 		System.out.println("Enter the Guest mobile number:");
 		String mobileNumber = sc.nextLine();
 		
-		//Compare with guest
+		/**
+		 * Compare with guest
+		 */
 		for (int i=0; i<hotel.getGuestList().size(); i++) {
 			Guest curGuest = hotel.getGuestList().get(i);
 			if (guestName.compareTo(curGuest.getName().toLowerCase())==0 && mobileNumber.compareTo(curGuest.getContacts().getMobileNumber())==0) {
