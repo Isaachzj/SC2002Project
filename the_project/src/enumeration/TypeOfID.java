daf4c5aae00c2b4627bcd7d6ecdd0e96f89cd03e
Tomoki Teng <103061517+TomokiTeng@users.noreply.github.com>
2022-04-12 15:37:53 +0800

    all changes and explanation made accordingly w justifications

--- the_project/src/enumeration/TypeOfID.java
+++ the_project/src/enumeration/TypeOfID.java
@@ -1,5 +1,12 @@
 package enumeration;
 
 public enum TypeOfID {
+	/**
+	 * For the enumeration of the TypeOfID, there are 2 types (DRIVING_LICENSE, NRIC, PASSPORT)
+	 * 
+	 * DRIVING_LICENSE is used to indicate that the identification type for the guest is taken from their driver's license
+	 * NRIC is used to indicate that the identification type for the guest is taken from their NRIC (National Registration Identity Card)
+	 * PASSPORT is used to indicate that the identification type for the guest is taken from their passport
+	 */
 	DRIVING_LICENSE, NRIC, PASSPORT
 }
--- the_project/src/enumeration/TypeOfRoom.java
+++ the_project/src/enumeration/TypeOfRoom.java
@@ -1,5 +1,14 @@
 package enumeration;
 
 public enum TypeOfRoom {
+	/**
+	 * For the enumeration of the TypeOfRoom, there are 4 types (SINGLE, DOUBLE, DELUXE, VIPSUITE)
+	 * 
+	 * SINGLE is used to indicate when the room size is of Single
+	 * DOUBLE is used to indicate when the room size is of Double
+	 * DELUXE is used to indicate when the room size is of Deluxe
+	 * VIPSUITE is used to indicate when the room size is of VIP Suite
+
+	 */
 	SINGLE, DOUBLE, DELUXE, VIPSUITE
 }
