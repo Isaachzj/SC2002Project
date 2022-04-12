e834777f51da6b78b339f1aa2f4eb55ce6e56ec4
Tomoki Teng <103061517+TomokiTeng@users.noreply.github.com>
2022-04-12 12:28:09 +0800

    explanation

--- the_project/src/list_methods/AddGivenObject.java
+++ the_project/src/list_methods/AddGivenObject.java
@@ -1,5 +1,10 @@
 package list_methods;
 
 public interface AddGivenObject {
+	/**
+	 * calls addEntry method which allows object to be added given no duplicate entry found
+	 * @param object this is the object to be added into the Entry
+	 * @throws ArrayException occurs upon a duplicate entry
+	 */
 	public void addEntry(Object object) throws ArrayException;
 }
