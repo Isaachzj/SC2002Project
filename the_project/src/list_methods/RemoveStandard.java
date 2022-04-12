97d877b2adb7d6f9e88a0db5d6d878ed0bbe5f55
Tomoki Teng <103061517+TomokiTeng@users.noreply.github.com>
2022-04-12 14:42:00 +0800

    javadocs; exception handling Removal method no entry found

--- the_project/src/list_methods/RemoveStandard.java
+++ the_project/src/list_methods/RemoveStandard.java
@@ -1,6 +1,13 @@
 package list_methods;
 
 public interface RemoveStandard {
-	//remove method
+	/**
+	 *  #remove method
+	 */
+	/**
+	 * Exception handling for entry removal
+	 * i.e. when no entry is found upon removal of entry; exception (ArrayException) would be thrown
+	 * @throws ArrayException occurs when no entry found for its request to be removed
+	 */
 	public void removeEntry() throws ArrayException;
 }
