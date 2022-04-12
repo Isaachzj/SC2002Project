133ee0aa4fcd8731f8c25102ceaa68ed6d47fa88
Tomoki Teng <103061517+TomokiTeng@users.noreply.github.com>
2022-04-12 14:53:48 +0800

    explanation

--- the_project/src/list_methods/Set.java
+++ the_project/src/list_methods/Set.java
@@ -2,5 +2,9 @@ package list_methods;
 
 public interface Set {
 	//set (override entry)
+	/**
+	 * calls set() method which overrides the entry in the array (with exception would be thrown for any error message encountered)
+	 * @throws ArrayException occurs when there are any errors upon the overriding of entry in the array
+	 */
 	public void set() throws ArrayException;
 }
