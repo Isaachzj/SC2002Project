1a08e88b9b1ad1b4f9498c332d335fcd0cb6f61b
Tomoki Teng <103061517+TomokiTeng@users.noreply.github.com>
2022-04-12 10:07:16 +0800

    Instantiation + javadoc formatting

--- the_project/src/guest/DisplayGuestInfo.java
+++ the_project/src/guest/DisplayGuestInfo.java
@@ -8,12 +8,18 @@ public class DisplayGuestInfo {
 	 * @param guest this is the guest that needs to update credit card information
 	 */
 	public static void printInfo(Guest guest) {
-	
+		public static void updateCreditCardInfo(Guest guest) {
+		/**
+		* Check if guest==null
+		*/
 		if (guest==null) {
 			System.out.println("Guest does not exist! (GHOSTTTTT)");
 			return;
 		}
-		
+		/**
+		 * Displays guest details
+		 * guest details include (name, nationality, gender, identification type and identification number, Credit card details and customer details)
+		 */
 		System.out.println("----------Guest Details---------");
 		System.out.println("Name: "+ guest.getName());
 		System.out.println("Nationality: " + guest.getNationality());
