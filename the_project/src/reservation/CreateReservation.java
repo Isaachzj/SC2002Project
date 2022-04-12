ecae17116b398adc8f1bfdc19a9a58cead95d1f6
Tomoki Teng <103061517+TomokiTeng@users.noreply.github.com>
2022-04-13 02:33:12 +0800

    instantiation and exception handling

--- the_project/src/reservation/CreateReservation.java
+++ the_project/src/reservation/CreateReservation.java
@@ -14,20 +14,42 @@ import room.*;
 import enumeration.AvailStatus;
 
 public class CreateReservation {
+	/**
+	 * Instantiation of the Reservation object
+	 * this includes details such as the room, number of guest, check-in date-time and also check-out date-time
+	 * 
+	 * @param room this is the Room object of the reservation
+	 * @param numOfGuest this is the number of guest that would be staying in the hotel for the reservation
+	 * @param checkInDateTime this is the check in date of the reservation.
+	 * The check in date will always be before the check out date.
+	 * @param checkOutDateTime this is the check out date of the reservation.
+	 * The check out date will always be after the check in date.
+	 * @return the Reservation object; createReservation would be instatiated and returned.
+	 * The returned Reservation object to add to reservation list in Hotel object
+	 * @throws ArrayException occurs upon an error and error message would be printed accordingly
+	 */
 	public static Reservation createReservation(Room room, int numOfGuest, LocalDateTime checkInDateTime, LocalDateTime checkOutDateTime) throws ArrayException{
 		Scanner sc = new Scanner(System.in);
 
 		long numOfWeekday = LengthOfStay.calcWeekDays(checkInDateTime.toLocalDate(), checkOutDateTime.toLocalDate());
 		long numOfWeekend = LengthOfStay.calcWeekEnds(checkInDateTime.toLocalDate(), checkOutDateTime.toLocalDate());
 		
-		//Create Reservation Object
+		/**
+		 * # Create Reservation Object
+		 */
 		Reservation reservation = new Reservation(room, checkInDateTime, checkOutDateTime, numOfWeekday, numOfWeekend, numOfGuest);
-		//Add guest to reservation guest list
+		/**
+		 * # Add guest to reservation guest list
+		 */
 
-		//Creates guests and add guests to reservation guest list
+		/**
+		 * # Creates guests and add guests to reservation guest list
+		 */
 		reservation.addGuest();
 		
-		//passes Reservation object to the room if the room was originally vacant (no existing reservations)
+		/**
+		 * # passes Reservation object to the room if the room was originally vacant (no existing reservations)
+		 */
 		if(room.getReservation()==null) {
 			room.setReservation(reservation);
 			room.setAvail(AvailStatus.RESERVED);
