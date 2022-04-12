b41e4a2b21dc159e6075a4018e68269bdb6d284e
Tomoki Teng <103061517+TomokiTeng@users.noreply.github.com>
2022-04-13 03:25:07 +0800

    zTO B CONTINUED

--- the_project/src/room/DeluxeRoom.java
+++ the_project/src/room/DeluxeRoom.java
@@ -9,14 +9,20 @@ public class DeluxeRoom extends Room{
 	public DeluxeRoom(Menu menu, int floor, int num) {
 		super(menu);		
 		
-		// Instantiating necessary objects
+		/**
+		 * # Instantiating necessary objects
+		 */
 		RoomFeatures ff = new RoomFeatures(TypeOfBed.QUEEN , ViewType.CITY, 1, true, false);
 		RoomRate rr = new RoomRate(50, 80, 0);
 		
-		//Overriding attributes of super()
+		/**
+		 * # Overriding attributes of super()
+		 */
 		this.roomType = TypeOfRoom.DELUXE;
 		
-		//Getting the correct form for room number
+		/**
+		 * # Getting the correct form for room number
+		 */
 		if (num<10)
 			this.roomNumber = '0'+String.valueOf(floor)+'0'+String.valueOf(num);
 		else
