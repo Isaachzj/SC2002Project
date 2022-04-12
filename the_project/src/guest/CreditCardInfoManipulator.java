b65002ae1b15978c78f468b52cb42512aba0b508
Tomoki Teng <103061517+TomokiTeng@users.noreply.github.com>
2022-04-12 10:04:08 +0800

    Instantiation Explained

--- the_project/src/guest/CreditCardInfoManipulator.java
+++ the_project/src/guest/CreditCardInfoManipulator.java
@@ -4,17 +4,22 @@ import java.util.Scanner;
 public class CreditCardInfoManipulator {
 	//CreditCardInfo CCinfo = new CreditCardInfo(CCNum, CCBank, ExpiryDate, BillName, BillAddress);
 	/**
-	 * Updates the credit card information of the guest; with the input of the credit card number, bank, expiry datem billing name and billing address
-	 * If parameter for Guest object is null; to return. Else, if Guest object found will update accordingly
+	 * Updates the credit card information of the guest; with the input of the guest's credit details (card number, bank, expiry datem billing name and billing address)
+	 * If parameter for Guest object is null; the method would be returned.
+	 * Else, if Guest object found; its credit card information (CreditCardInfo Object) would be updated accordingly
 	 * @param guest this is the guest that needs to update credit card information
 	 */
 	public static void updateCreditCardInfo(Guest guest) {
-		// Check if guest==null
+		/**
+		 * Check if guest==null
+		 */
 		if (guest==null) {
 			System.out.println("Guest does not exist! (GHOSTTTTT)");
 			return;
 		}
-		// Updating CreditCardInfo Object
+		/**
+		 * Updating CreditCardInfo Object
+		 */
 			Scanner sc = new Scanner(System.in);
 			System.out.println("--------- Enter Credit Card details --------");
 			System.out.print("Enter your new creditcard Number: ");
