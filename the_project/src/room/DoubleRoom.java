ba8b4489101ec4d57df09063565bc1bb45ce0710
Tomoki Teng <103061517+TomokiTeng@users.noreply.github.com>
2022-04-14 15:14:45 +0800

    (instantiation)

--- the_project/src/room/DoubleRoom.java
+++ the_project/src/room/DoubleRoom.java
@@ -15,22 +15,27 @@ public class DoubleRoom extends Room{
 	 * # Constructor
 	 */
 	/**
-	 * Calls the Room constructor;
-	 * Sets this roomType to DOUBLE, sets this roomNumber, sets maxOccupancy of this Double room to 2, 
-	 * sets availability of this Double room to VACANT and menu of this Double room to Menu object passed in;
+	 * Instantiation of the DoubleRoom object
+	 * this includes details such as the menu available to the room, room floor and room number
+	 * for the instantiation of the DeluxeRoom object, we have set the max occupancy to 2 (i.e. maximum of 2 guests that can stay in a give Double Room)
 	 * 
-	 * @param menu Menu object containing food items available to this Double room for room service;
-	 * @param floor of type int here refers to the floor number of the room, used to initialize roomNumber;
-	 * @param num of type int here refers to the second number of the room, used to initialize roomNumber;
+	 * @param menu	this is the Menu object containing Food available for the Double room
+	 * Menu object containing food items available to this Double room for room service;
+	 * @param floor	this is the room floor, which is required for the instantiation of the DoubleRoom object
+	 * @param num this is the room number, which is required for the instantiation of the DoubleRoom object
 	 */
 	public DoubleRoom(Menu menu, int floor, int num) {
 		
 		super(menu, TypeOfBed.SINGLE, ViewType.STANDARD, 2, true, false, 80, 100);			
 
-		//Overriding attributes of super()
+		/**
+		 * # Overriding attributes of super()
+		 */
 		this.roomType = TypeOfRoom.DOUBLE;
 		
-		//Getting the correct form for room number
+		/**
+		 * # Getting the correct form for room number
+		 */
 		if (num<10)
 			this.roomNumber = '0'+String.valueOf(floor)+'0'+String.valueOf(num);
 		else
