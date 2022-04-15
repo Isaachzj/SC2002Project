e1e8128ab0a8b7cfb4934b3e5e658eec975f18d0
ngwenlu <97866952+ngwenlu@users.noreply.github.com>
2022-04-15 09:38:43 +0800

    checkin stayrelated roomrelated

--- the_project/src/app_hotel/StayRelated.java
+++ the_project/src/app_hotel/StayRelated.java
@@ -8,9 +8,22 @@ import app_room.UpdateRoomStatus;
 import hotel.Hotel;
 import list_methods.ArrayException;
 
-public class StayRelated {
+/**
+ * This class allows the user to make access functions which are related to guests staying in a hotel room 
+ * through a switch statement and input choice;
+ * A do-while loop is used to ensure that the guest enters a valid choice (1 - 3)
+ * Functions include making a reservation (option 1), checking in a guest (option 2) and checking out a guest (option 3);
+ */
+public class StayRelated { 
+	
+	/**
+	 * Allows user to access functions which are related to guests staying in a hotel room;
+	 * @param hotel Hotel object containing all room, guests, room service orders and reservations details;
+	 * @throws ArrayException
+	 */
 	public static void stayRelated(Hotel hotel) throws ArrayException {
-		/*Contains switch statement that:
+		/**
+		 * # Contains switch statement that:
 			1. Make a Reservation
 			2. Check-In a guest
 			3. Check-Out a guest
