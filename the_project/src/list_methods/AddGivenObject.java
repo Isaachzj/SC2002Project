383c0ec6254db4c290d0877ca038182bb758d469
Tomoki Teng <103061517+TomokiTeng@users.noreply.github.com>
2022-04-16 17:09:10 +0800

    fixed javadocs

--- the_project/src/list_methods/AddGivenObject.java
+++ the_project/src/list_methods/AddGivenObject.java
@@ -1,5 +1,16 @@
 package list_methods;
-
+/**
+ * Follows the Interface Segregation Principle
+ * This is part of the smaller interface; from the Add interface
+ * 
+ * calls addEntry method which allows object to be added given no exception thrown
+ * (i.e. given no errors)
+ * 
+ * @author Isaac, Yan kai, Davis, Wenlu, Tomoki
+ * @version 7.0
+ * @since 2022-03-15
+ *
+ */
 public interface AddGivenObject {
 	/**
 	 * calls addEntry method which allows object to be added given no exception thrown
--- the_project/src/list_methods/AddStandard.java
+++ the_project/src/list_methods/AddStandard.java
@@ -1,5 +1,17 @@
 package list_methods;
-
+/**
+ * Follows the Interface Segregation Principle
+ * This is part of the smaller interface; from the Add interface
+ * 
+ * Exception handling for duplicate entry
+ * i.e. when a duplicate entry is found upon adding of entry; exception
+ * (ArrayException) would be thrown
+ * 
+ * @author Isaac, Yan kai, Davis, Wenlu, Tomoki
+ * @version 7.0
+ * @since 2022-03-15
+ *
+ */
 public interface AddStandard {
 	/**
 	 * Exception handling for duplicate entry
