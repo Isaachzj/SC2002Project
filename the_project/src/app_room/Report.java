155a1258aaed1b0cf9c48ece65c3e6c758c3927c
Tomoki Teng <103061517+TomokiTeng@users.noreply.github.com>
2022-04-16 09:45:49 +0800

    included explanation and breakdown step

--- the_project/src/app_room/Report.java
+++ the_project/src/app_room/Report.java
@@ -2,7 +2,21 @@ package app_room;
 
 import hotel.*;
 import java.util.*;
-
+/**
+ * This is a Boundary Class; as it is used as a user interface;
+ * 
+ * Allows the Occuapncy report and the Status report to be displayed
+ * Given an option to: print occupancy report (option 1) or print status report (option 2)
+ * 
+ * printOccupancyReport (option 1) displays a summary of the occupancy details of the rooms - 
+ * holds the list of vacant rooms for each Room Type
+ * and counts the number of room added to each array element and total number of each type of room available
+ * printStatusReport (option 2) displays a summary of the status of rooms -
+ * holds the list of rooms of each availability type
+ * and counts the number of room added to each array element
+ * 
+ * @author Isaac, Yan kai, Davis, Wenlu, Tomoki
+ */
 public class Report {
 	
 	public static void info(Hotel hotel) {
