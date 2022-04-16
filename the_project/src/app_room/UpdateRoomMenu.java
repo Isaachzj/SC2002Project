a6a8c6cc1fbcd39463db00035bc30b7a4accc71b
Tomoki Teng <103061517+TomokiTeng@users.noreply.github.com>
2022-04-16 18:50:35 +0800

    Added and fixed javadog

--- the_project/src/app_room/UpdateRoomMenu.java
+++ the_project/src/app_room/UpdateRoomMenu.java
@@ -10,10 +10,38 @@ import room.Room;
 import list_methods.*;
 import java.util.*;
 
+/**
+ * This is a Boundary Class; as it is used as a user interface;
+ * 
+ * Interface which displays a menu that allows the user to access functions
+ * related to menus rooms can access;
+ * user is able to add a new food item (option 1;
+ * addFood() method is called to add a new Food object into the menu),
+ * remove a food item (option 2;
+ * removeFood() method is called to remove an existing Food object from the menu), 
+ * and, update the quantity of a food item(option 3;
+ * updateFood() method is called to make changed to an existing Food object
+ * from the menu. Notably, this allows the user to make changes to either the name
+ * of the food, its prices or both).
+ * 
+ * 
+ * @author Isaac, Yan kai, Davis, Wenlu, Tomoki
+ * @version 7.0
+ * @since 2022-03-15
+ */
 public class UpdateRoomMenu {
+	/**
+	 * Method allows users to be able to user is able to add a new food item (option 1),
+	 * remove a food item (option 2), 
+	 * and, update the quantity of a food item(option 3)
+	 * Notably, this allows the user to make changes to either the name
+	 * of the food, its prices or both).
+	 */
 	public static void updateRoomMenu(Hotel hotel) throws ArrayException {
 		Scanner sc = new Scanner(System.in);
-		//Ask for room type (Not hardcoded!)
+		/**
+		 * # Ask for room type (Not hardcoded!)
+		 */
 		System.out.println("Choose Room Type (Select option):\n-Enter an alphabet to terminate-");
 		for (int i=0; i<TypeOfRoom.values().length; i++) {
 			System.out.printf("%d. %s\n", i+1, TypeOfRoom.values()[i]);
@@ -24,10 +52,14 @@ public class UpdateRoomMenu {
 			typeChoice = sc.nextInt(); sc.nextLine();
 		}
 		
-		//Getting the menu
+		/**
+		 * # Getting the menu
+		 */
 		Menu menu = hotel.getMenu(typeChoice);
 		
-		//Select what u wanna do with the menu
+		/**
+		 * # Select what u wanna do with the menu
+		 */
 		System.out.println("Choose an option: \nEnter an alphabet to terminate-:");
 		System.out.println("1. Add Food\n2. Remove Food\n3. Update Food Price and/or Description");
 		int choice = sc.nextInt(); sc.nextLine();
