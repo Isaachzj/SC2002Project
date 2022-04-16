988b4ada1dfbfafe98c9e0d13ae0e223cefa964a
Tomoki Teng <103061517+TomokiTeng@users.noreply.github.com>
2022-04-16 19:30:16 +0800

    java docs interface x user gapokem

--- the_project/src/list_methods/ArrayException.java
+++ the_project/src/list_methods/ArrayException.java
@@ -1,5 +1,17 @@
 package list_methods;
-
+/**
+ * Follows the Interface Segregation Principle
+ * Many smaller interface; inwhich this case is
+ * an extension of Exception class with methods of ArrayException
+ * Uses of method overloading; if ArrayException does not contain
+ * a message in its parameter, throw an Error message upon exception.
+ * Else, if ArrayException contains a message in its parameter;
+ * throw the given error message upon exception
+ * 
+ * @author Isaac, Yan kai, Davis, Wenlu, Tomoki
+ * @version 7.0
+ * @since 2022-03-15
+ */
 public class ArrayException extends Exception {
 	/**
 	 *  Exception Handling
